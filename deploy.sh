631add538b79373ebe85df7c87c39683cb476fd2
igle <Dubsjules@gmail.com>
2022-05-27 20:35:01 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@ echo "Deploying changes..."
 # Pull changes from the live branch
 git pull
 
-# Shut down the existing containers
-docker-compose -f docker-compose.prod.yml down
-
 # Start the new containers
 docker-compose -f docker-compose.prod.yml up --build -d
 echo "Deployed!"
